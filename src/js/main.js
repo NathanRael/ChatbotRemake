2951c8663f83567277ec75758d9410f0678ea66e
NathanRael <ralaivoavy.natanael@gmail.com>
2023-11-02 11:43:26 +0300

    using API

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-
 /******Toggle index popup **********/
 
 class ToggleMainButton {
@@ -7,44 +6,43 @@ class ToggleMainButton {
     popupName;
     fadeField;
 
-    constructor (buttonName, popupName, fadeField){
+    constructor(buttonName, popupName, fadeField) {
         this.buttonName = buttonName;
         this.popupName = popupName;
         this.fadeField = fadeField;
     }
 
-    Animate  () {
-        $(this.buttonName).click(  ()=> {
+    animate() {
+        $(this.buttonName).click(() => {
             !this.is_popup_toggled ? this.#togglePopup(this.popupName, 0, true) : null;
             this.#toggleFadeField();
-        })
+        });
     }
 
-    #togglePopup(popupName, postion, is_toggled){
+    #togglePopup(popupName, position, isToggled) {
         $(popupName).show().animate({
-            "right" : postion,
+            "right": position,
         }, 500);
-        this.is_popup_toggled = is_toggled;
+        this.is_popup_toggled = isToggled;
     }
 
-    #toggleFadeField(){
-        if (this.is_popup_toggled){
-
+    #toggleFadeField() {
+        if (this.is_popup_toggled) {
             $(this.fadeField).show()
-            .click( () => {
-                this.#togglePopup(this.popupName, '-524px', false);
-                $(this.fadeField).hide();
-            });
+                .click(() => {
+                    this.#togglePopup(this.popupName, '-524px', false);
+                    $(this.fadeField).hide();
+                });
         }
     }
-
 }
 
-let loginBtn = new ToggleMainButton('.btn-login', '.login-popup', '.fade-field' );
-let signupBtn = new ToggleMainButton('.btn-signup', '.signup-popup', '.fade-field' );
+// Create instances and call methods
+let loginBtn = new ToggleMainButton('.btn-login', '.login-popup', '.fade-field');
+let signupBtn = new ToggleMainButton('.btn-signup', '.signup-popup', '.fade-field');
 
-loginBtn.Animate();
-signupBtn.Animate();
+loginBtn.animate();
+signupBtn.animate();
 
 
 /******toggle setting&hitory popup**** */
@@ -52,79 +50,74 @@ signupBtn.Animate();
 class MovingPart {
     itemName;
 
-    constructor (itemName){
+    constructor(itemName) {
         this.itemName = itemName;
-
     }
 
-    handleClick(speed = 500, popupName = '.setting-popup', popupBg = '.popup-bg'){
-        $(this.itemName).click(() =>{
+    handleClick(speed = 500, popupName = '.setting-popup', popupBg = '.popup-bg') {
+        $(this.itemName).click(() => {
             $(popupName).show(speed).css('display', 'flex');
-            $(popupBg).show(speed/5);
-        })
+            $(popupBg).show(speed / 5);
+        });
     }
 
-    canBeClosed(closeIcon, speed = 500, popupName = '.setting-popup', popupBg = '.popup-bg'){
-        $(closeIcon).click(() =>{
+    canBeClosed(closeIcon, speed = 500, popupName = '.setting-popup', popupBg = '.popup-bg') {
+        $(closeIcon).click(() => {
             $(popupName).hide(speed);
-            $(popupBg).hide(speed/5);
-        })
-
+            $(popupBg).hide(speed / 5);
+        });
     }
 
-    toggleItem(){
+    toggleItem() {
         let is_clicked = true;
-        $('.user-abbreviation').click(()=>{
-            if(is_clicked){
+        $('.user-abbreviation').click(() => {
+            if (is_clicked) {
                 is_clicked = false;
                 $('.user-setting, .user-logout, .new-chat, .chat-history')
-                .slideToggle(500, () => is_clicked = true)//using callback function
-                .css('display', 'flex');
+                    .slideToggle(500, () => is_clicked = true)
+                    .css('display', 'flex');
             }
-        })
+        });
     }
 
-    canBeDragged(anchor = this.itemName, container = '.user-item-container'){//dragging the object depending to the cursor pos
-        $(anchor).mousedown(function(e) {
-            e.preventDefault(); // Prevents default behavior (e.g., text selection)
-        
+    canBeDragged(anchor = this.itemName, container = '.user-item-container') {
+        $(anchor).mousedown(function (e) {
+            e.preventDefault();
             let element = $(container);
             let isDragging = true;
-    
-            $(document).mousemove(function (e){
-                if (isDragging){
+
+            $(document).mousemove(function (e) {
+                if (isDragging) {
                     element.css({
                         'top': e.pageY - $(window).scrollTop() - 120,
                         'left': e.pageX - 120,
                     });
-                    /****Reseting the movingPart pos if it's far from the windows */
-                    if (e.pageY <= 64){
+                    if (e.pageY <= 64) {
                         element.css({
-                            'top':  $(window).scrollTop() - 0,
+                            'top': $(window).scrollTop() - 0,
                             'left': e.pageX - 120,
                         });
                     }
-                    if(e.pageX <= 0){
+                    if (e.pageX <= 0) {
                         element.css({
                             'top': e.pageY - $(window).scrollTop() - 120,
                             'left': -128,
-                        }); 
+                        });
                     }
                 }
-            }).mouseup(function() {
+            }).mouseup(function () {
                 isDragging = false;
                 $(document).off('mousemove').off('mouseup');
             });
-    
-        })
-    
-    } 
 
+        });
+    }
 }
 
+// Create instances and call methods
 let userSetting = new MovingPart('.user-setting');
 let chatHistory = new MovingPart('.chat-history');
-let userAbbreviation = new MovingPart('.user-abbreviation')
+let userAbbreviation = new MovingPart('.user-abbreviation');
 
 userSetting.handleClick();
 userSetting.canBeClosed('.close-popup');
@@ -133,53 +126,51 @@ chatHistory.canBeClosed('.close-popup');
 userAbbreviation.canBeDragged();
 userAbbreviation.toggleItem();
 
-$('.new-chat').click( () =>{
-    // window.location.href('')
+$('.new-chat').click(() => {
     alert('New chat');
-    }
-)
+});
 
-$('.user-logout').click(()=>{
+$('.user-logout').click(() => {
     window.location.href = '/index.html';
-})
-
+});
 
 /***Input ****/
-class Message{
+class Message {
     input;
     button;
-    message;
 
-    constructor (input, button){
+    constructor(input, button) {
         this.input = input;
+        this.button = button;
     }
 
-    sendMessage (from = 'other', data, dataName){
-        if (from === 'other'){
-            if (data){
+    sendMessage(from = 'other', data, dataName) {
+        if (from === 'other') {
+            if (data) {
                 this.saveData(data, dataName);
                 this.clearInputValue();
-                this.setUserMessage(dataName);
-                this.setBotMessage();
-                
-            }else{
+                this.renderUserMessage(dataName);
+                this.generateBotMessage(data);//THE BOT REPLY
+
+            } else {
                 alert("Please type something before you send !");
             }
-            
-        }else if (from = 'main'){
-            if (data){
+
+        } else if (from === 'main') {
+            if (data) {
                 this.saveData(data, dataName);
                 window.location.href = '/problemSolverInterface.html';
 
-            }else{
+            } else {
                 alert("Please type something before you send !");
             }
         }
     }
 
-    setUserMessage(dataName){
+    renderUserMessage(dataName) {
+
         let message = this.loadData(dataName) || 'undefined';
-        if (message){
+        if (message) {
             $('.reply-container').append(`
                 <li class="user-reply">
                     <p class="user-reply-abbreviation">R</p>
@@ -187,14 +178,12 @@ class Message{
                         ${message}
                     </div>
                 </li>
-            `
-            )
+            `);
         }
     }
 
-    setBotMessage(time = 500){
-        let message = 'yah';
-        setTimeout(()=>{
+    renderBotMessage( message,time = 500,) {
+        setTimeout(() => {
             $('.reply-container').append(`
             <li class="bot-reply">
                 <img src="./src/image/logo.png" alt="" width="125px">
@@ -204,69 +193,96 @@ class Message{
                     </p>
                 </div>
             </li>
-        `)
-        }, time)
-
+        `);
+        }, time);
     }
 
-    Click(func){
+    async generateBotMessage(userMessage) {
+
+        const apiUrl = 'https://api-fakell.x10.mx/v1/chat/completions/';
+    
+        const data = {
+            model: 'gpt-3.5-turbo',
+            messages: [
+                { role: 'user', content: userMessage }
+            ],
+            stream: false
+        };
+    
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+    
+        try {
+            const response = await fetch(apiUrl, requestOptions);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const responseData = await response.json();
+
+            let message = responseData.choices[0].message.content;
+
+            this.renderBotMessage(message);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+    
+    
+
+    
+    click(func) {
         $(this.button).click(func);
     }
 
-    clearInputValue(){
+    clearInputValue() {
         $(this.input).val('');
     }
 
-    saveData (data, dataName){
-        localStorage.setItem(dataName, data)
+    saveData(data, dataName) {
+        localStorage.setItem(dataName, data);
     }
 
-    loadData (dataName){
+    loadData(dataName) {
         return localStorage.getItem(dataName);
     }
-
 }
 
-
-//rendering the suggestionPromt inside the input
-
-$('.prompt-example').click(function (){
+// Render suggestionPrompt inside the input
+$('.prompt-example').click(function () {
     const prompt = $(this).text().trim() || null;
-    if (prompt){
+    if (prompt) {
         $('.user-input').val(prompt);
     }
+});
 
-})
-
+// Create instances and set up message sending
 let mainPrompt = new Message('#mainPromptInput', '#sendMainPrompt');
-let problePrompt = new Message('#probPromptInput', '#sendProbPrompt');
-let weatherApi = new Message('#ApiPromptInput', '#sendApiPrompt');
+let probPrompt = new Message('#probPromptInput', '#sendProbPrompt');
+let apiPrompt = new Message('#ApiPromptInput', '#sendApiPrompt');
+let userPrompt;
+
+$('#sendMainPrompt').click(function () {
+    userPrompt = $('#mainPromptInput').val().trim();
+    mainPrompt.sendMessage('main', userPrompt, 'mainMessage');
+
+});
+
+mainPrompt.renderUserMessage('mainMessage');
+mainPrompt.generateBotMessage(userPrompt);
 
 
 
-$('#sendMainPrompt').click(function () {//the button in the main UI
-    const prompt = $('#mainPromptInput').val().trim();
-    mainPrompt.sendMessage('main',prompt, 'mainMessage');
-    // messageSent = true;
-    
-})
+$('#sendProbPrompt').click(function () {
+    userPrompt = $('#probPromptInput').val().trim();
+    probPrompt.sendMessage('other', userPrompt, 'mainMessage');
+});
 
-mainPrompt.setBotMessage();
-
-mainPrompt.setUserMessage('mainMessage');
-
-$('#sendProbPrompt').click(function (){//the btn in the problemSolver UI
-    const prompt = $('#probPromptInput').val().trim();
-    problePrompt.sendMessage('other', prompt, 'mainMessage');
-
-})
-
-
-
-$('#sendApiPrompt').click(function (){//the btn in the problemSolver UI
-    const prompt = $('#ApiPromptInput').val().trim();
-    weatherApi.sendMessage('other', prompt, 'apiMessage');
-})
-
-
-
+$('#sendApiPrompt').click(function () {
+    userPrompt = $('#ApiPromptInput').val().trim();
+    apiPrompt.sendMessage('other', userPrompt, 'apiMessage');
+});
